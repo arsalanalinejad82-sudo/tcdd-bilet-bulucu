b71a59ef145215c97d326586894d0b10923a7418
arsalanalinejad82-sudo <arsalan.alinejad82@gmail.com>
2026-07-30 22:44:02 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,9 @@
-# Journey details (check stations.json for valid station names)
-binis_istasyon_adi = "İstanbul(Söğütlüçeşme)"
-inis_istasyon_adi = "Ankara Gar"
-date = "2024-02-02"
-
-# If you want to check all journeys in a given date/day, set to False. 
-# Else set to True and give a specific valid journey hour.
-check_specific_hour = False
-hour = "08:02"
-
-# Email details (replace with the email address (must be OUTLOOK) and password that will send notification
-# and email addres that will be notified)
-email_address = "sender_mail@outlook.com" 
-email_password = "sender_password"
-destination_address = "receiver_mail@any_mail.com"
-
-# Wait time between checks in seconds
+routes = [
+    ("İstanbul(Söğütlüçeşme)", "Eskişehir"),
+    ("İstanbul(Bostancı)", "Eskişehir"),
+]
+date = "2026-08-01"
+end_hour = "19:05"
+telegram_bot_token = "8636170666:AAG1n_rS2gsPvsC5IT_CkJsrBebJtAtMuj0"
+telegram_chat_id = "109706681"
 sleep_time = 10
