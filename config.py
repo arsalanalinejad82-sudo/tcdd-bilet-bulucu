abb18a4c5f78612d1eeec9c855e3b4f44560f693
arsalanalinejad82-sudo <arsalan.alinejad82@gmail.com>
2026-08-06 16:11:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 routes = [
-    ("İstanbul(Söğütlüçeşme)", "Eskişehir"),
-    ("İstanbul(Bostancı)", "Eskişehir"),
+    ("Eskişehir", "İstanbul(Bostancı)"),
 ]
-date = "2026-08-01"
-end_hour = "19:05"
+date = "2026-08-08"
+end_hour = "23:59"
 telegram_bot_token = "8636170666:AAG1n_rS2gsPvsC5IT_CkJsrBebJtAtMuj0"
 telegram_chat_id = "109706681"
 sleep_time = 10
